08b1a5b31bd203522fb4464b0f45a57104419f11
checkstock <50084326+checkstock@users.noreply.github.com>
2021-05-05 18:01:04 -0700

    Update day3

--- day3/day3.py
+++ day3/day3.py
@@ -1,45 +1,39 @@
+def find_trees(input, slope):
+    rows = len(input)
+    columns = len(input[0])
+
+    row_index = 0
+    column_index = 0
+
+    row_move = slope[1]
+    column_move = slope[0]
+
+    trees = 0
+
+    while row_index < rows:
+        if input[row_index][column_index] == '#':
+            trees += 1
+        row_index += row_move
+        column_index = (column_index + column_move) % columns
+
+    return trees
+
+def find_tree_products(input, slopes):
+    product = 1
+
+    for i in range(0, len(slopes)):
+        product *= find_trees(input, slopes[i])
+
+    return product
+
 input = []
 
 with open("input.txt") as file:
     for line in file:
         input.append(line.strip())
 
-rows = len(input)
-columns = len(input[0])
-
-print("Day 3 Part 1: ")
-column_index = 0
-trees = 0
-
-for i in range(0, rows):
-        if input[i][column_index] == '#':
-            trees += 1
-
-        column_index = (column_index + 3) % columns
-
-print("We encountered", trees, "trees.\n")
+print("Day 3 Part 1:")
+print("We encountered", find_trees(input, [3, 1]), "trees.")
 
 print("Day 3 Part 2:")
-product = 1
-slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
-
-for i in range(0, len(slopes)):
-    column_index = 0
-    row_index = 0
-    trees = 0
-
-    row_move = slopes[i][1]
-    column_move = slopes[i][0]
-
-    while row_index < rows:
-        if input[row_index][column_index] == '#':
-            trees += 1
-
-        row_index += row_move
-        column_index = (column_index + column_move) % columns
-
-    print("The number of trees in slope", column_move, "," , row_move, "is", trees)
-
-    product *= trees
-
-print("The product of the number of trees we encountered in the 5 slopes is:", product)
+print("The product of all the trees in our 5 slopes is:", find_tree_products(input, [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]))
